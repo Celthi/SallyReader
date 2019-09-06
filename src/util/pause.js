231534a9f414a99c8229508359afd6d8d5509bfe
evan l00 <evan_l00@qq.com>
2019-09-06 16:41:33 +0800

    structure as typescript

--- src/util/pause.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var pause = function (time) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, time);
-    });
-};
-exports.pause = pause;
-//# sourceMappingURL=pause.js.map
--- test/parseBookUrl.js
+++ test/parseBookUrl.js
@@ -2,33 +2,33 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var Books_1 = require("../src/Books");
-var Chapter_1 = require("../src/Chapter");
-var chai_1 = __importDefault(require("chai"));
-describe('parse book', function () {
-    it('should parse book', function () {
-        var book = Books_1.parseBookUrl('https://learning.oreilly.com/library/view/head-first-design/0596007124/');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Books_1 = require("../src/Books");
+const Chapter_1 = require("../src/Chapter");
+const chai_1 = __importDefault(require("chai"));
+describe('parse book', () => {
+    it('should parse book', () => {
+        const book = Books_1.parseBookUrl('https://learning.oreilly.com/library/view/head-first-design/0596007124/');
         console.log(book);
-        chai_1["default"].assert.deepEqual(book, {
+        chai_1.default.assert.deepEqual(book, {
             bookName: 'head-first-design',
             bookSeq: '0596007124',
             bookFolder: 'head-first-design/'
         });
     });
-    it('should parse chapter url', function () {
-        var chapter = Chapter_1.parseChapterUrl('/library/view/head-first-design/0596007124/ch09.html#reworking_the_diner_menu_with_iterator');
+    it('should parse chapter url', () => {
+        const chapter = Chapter_1.parseChapterUrl('/library/view/head-first-design/0596007124/ch09.html#reworking_the_diner_menu_with_iterator');
         console.log(chapter);
-        chai_1["default"].assert.deepEqual(chapter, {
+        chai_1.default.assert.deepEqual(chapter, {
             chapterName: 'ch09.html',
             chapterUrl: '/library/view/head-first-design/0596007124/ch09.html',
             book: 'head-first-design'
         });
     });
-    it('should parse chapter url', function () {
-        var chapter = Chapter_1.parseChapterUrl('/library/view/head-first-design/0596007124/ch09.html');
+    it('should parse chapter url', () => {
+        const chapter = Chapter_1.parseChapterUrl('/library/view/head-first-design/0596007124/ch09.html');
         console.log(chapter);
-        chai_1["default"].assert.deepEqual(chapter, {
+        chai_1.default.assert.deepEqual(chapter, {
             chapterName: 'ch09.html',
             chapterUrl: '/library/view/head-first-design/0596007124/ch09.html',
             book: 'head-first-design'
